2b29122fbf94825bae0374f68aade20aae8aa333
cliffpeskin <cliffpeskin@gmail.com>
2024-06-21 16:04:46 -0300

    polishing homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,16 +5,16 @@ export default {
 			spacing: {
 				'18': '3rem', // 72px			
 			},
+			fontFamily: {
+				'sans': ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+				'oceanwide': ['Oceanwide', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+			  },
 			colors: {
 				blue: {
 					400: '#6EC4E9',
 					500: '#00A3E7',
 					600: '#0079AC',
 				}
-			},
-			fontSize: {
-				'6xl': '5em',
-				'7xl': '6em',
 			},
 		},
 	},
